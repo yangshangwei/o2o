2992c73988916009844ae7be1f433363c242077c
Mr.Yang <815150141@qq.com>
2018-05-14 00:38:56 -0400

    产品详情图片实体列及库表的创建

--- src/main/resources/DBInitScript/InitSQL.sql
+++ src/main/resources/DBInitScript/InitSQL.sql
@@ -167,3 +167,22 @@ CREATE TABLE `tb_product_category` (
   PRIMARY KEY (`product_category_id`),
   CONSTRAINT `fk_procate_shop` FOREIGN KEY (`shop_id`) REFERENCES `tb_shop` (`shop_id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
+
+
+
+--
+-- Table structure for table `tb_product_img`
+--
+
+DROP TABLE IF EXISTS `tb_product_img`;
+
+CREATE TABLE `tb_product_img` (
+  `product_img_id` int(20) NOT NULL AUTO_INCREMENT,
+  `img_addr` varchar(2000) NOT NULL,
+  `img_desc` varchar(2000) DEFAULT NULL,
+  `priority` int(2) DEFAULT '0',
+  `create_time` datetime DEFAULT NULL,
+  `product_id` int(20) DEFAULT NULL,
+  PRIMARY KEY (`product_img_id`),
+  CONSTRAINT `fk_proimg_product` FOREIGN KEY (`product_id`) REFERENCES `tb_product` (`product_id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
