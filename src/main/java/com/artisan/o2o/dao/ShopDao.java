9b109a3ba4977d3bd869c0561273371c2248c3cf
Mr.Yang <815150141@qq.com>
2018-05-18 03:20:17 -0400

    【商铺管理】DAO层-新增商铺

---
+++ src/main/java/com/artisan/o2o/dao/ShopDao.java
@@ -0,0 +1,18 @@
+package com.artisan.o2o.dao;
+
+import com.artisan.o2o.entity.Shop;
+
+public interface ShopDao {
+	/**
+	 * 
+	 * 
+	 * @Title: insertShop
+	 * 
+	 * @Description: 店铺注册
+	 * 
+	 * @param shop
+	 * 
+	 * @return: int 受影响的行数 1即为成功 -1(mybtis返回的值)失败
+	 */
+	int insertShop(Shop shop);
+}
---
+++ src/test/java/com/artisan/o2o/dao/ShopDaoTest.java
@@ -0,0 +1,62 @@
+package com.artisan.o2o.dao;
+
+import java.util.Date;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.artisan.o2o.BaseTest;
+import com.artisan.o2o.entity.Area;
+import com.artisan.o2o.entity.PersonInfo;
+import com.artisan.o2o.entity.Shop;
+import com.artisan.o2o.entity.ShopCategory;
+
+public class ShopDaoTest extends BaseTest {
+	
+	private static final Logger logger = LoggerFactory.getLogger(ShopDaoTest.class);
+
+	@Autowired
+	ShopDao shopDao;
+	
+	@Test
+	public void testQueryArea() {
+
+		Shop shop = new Shop();
+		PersonInfo personInfo = new PersonInfo();
+		Area area = new Area();
+		ShopCategory shopCategory = new ShopCategory();
+
+		// 因为tb_shop表中有外键约束,因此务必确保 设置的这几个id在对应的表中存在.
+		// 我们提前在tb_person_inf tb_area
+		// tb_shop_category分别添加了如下id的数据,以避免插入tb_shop时抛出如下异常
+		// com.mysql.jdbc.exceptions.jdbc4.MySQLIntegrityConstraintViolationException:
+		// Cannot add or update a child row: a foreign key constraint fails
+		// (`o2o`.`tb_shop`, CONSTRAINT `fk_shop_area` FOREIGN KEY (`area_id`)
+		// REFERENCES `tb_area` (`area_id`))
+		personInfo.setUserId(1L);
+		area.setAreaId(1);
+		shopCategory.setShopCategoryId(1L);
+
+		shop.setOwner(personInfo);
+		shop.setArea(area);
+		shop.setShopCategory(shopCategory);
+		shop.setShopName("Artisan");
+		shop.setShopDesc("ArtisanDesc");
+		shop.setShopAddr("NanJing");
+		shop.setPhone("123456");
+		shop.setShopImg("/xxx/xxx");
+		shop.setPriority(99);
+		shop.setCreateTime(new Date());
+		shop.setLastEditTime(new Date());
+		shop.setEnableStatus(0);
+		shop.setAdvice("Waring");
+		
+		int effectNum = shopDao.insertShop(shop);
+
+		Assert.assertEquals(effectNum, 1);
+		logger.debug("insert  successfully");
+	}
+}
