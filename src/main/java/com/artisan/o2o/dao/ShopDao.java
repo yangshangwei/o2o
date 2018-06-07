84927396f9157c61f231759d3bbcbe247b5f399e
Mr.Yang <815150141@qq.com>
2018-06-06 23:44:56 -0400

    【商铺列表】Dao层开发

--- src/main/java/com/artisan/o2o/dao/ShopDao.java
+++ src/main/java/com/artisan/o2o/dao/ShopDao.java
@@ -1,5 +1,9 @@
 package com.artisan.o2o.dao;
 
+import java.util.List;
+
+import org.apache.ibatis.annotations.Param;
+
 import com.artisan.o2o.entity.Shop;
 
 public interface ShopDao {
@@ -42,4 +46,40 @@ public interface ShopDao {
 	 * @return: Shop
 	 */
 	Shop selectShopById(long shopId);
+
+	/**
+	 * 
+	 * 
+	 * @Title: selectShopList
+	 * 
+	 * @Description: 带有分页功能的查询商铺列表 。
+	 * 
+	 *               可输入的查询条件：商铺名（要求模糊查询） 区域Id 商铺状态 商铺类别 owner
+	 *               (注意在sqlmapper中按照前端入参拼装不同的查询语句)
+	 * 
+	 * 
+	 * @param shopConditionShop
+	 * @param rowIndex
+	 *            从第几行开始取
+	 * @param pageSize
+	 *            返回多少行数据（页面上的数据量）
+	 * 
+	 *            比如 rowIndex为1,pageSize为5 即为 从第一行开始取，取5行数据
+	 * 
+	 * @return: List<Shop>
+	 */
+	List<Shop> selectShopList(@Param("shopCondition") Shop shopCondition, @Param("rowIndex") int rowIndex, @Param("pageSize") int pageSize);
+
+	/**
+	 * 
+	 * 
+	 * @Title: selectShopCount
+	 * 
+	 * @Description: 按照条件查询 符合前台传入的条件的商铺的总数
+	 * 
+	 * @param shopCondition
+	 * 
+	 * @return: int
+	 */
+	int selectShopCount(@Param("shopCondition") Shop shopCondition);
 }
--- src/test/java/com/artisan/o2o/dao/ShopDaoTest.java
+++ src/test/java/com/artisan/o2o/dao/ShopDaoTest.java
@@ -1,6 +1,7 @@
 package com.artisan.o2o.dao;
 
 import java.util.Date;
+import java.util.List;
 
 import org.junit.Assert;
 import org.junit.Test;
@@ -107,4 +108,127 @@ public class ShopDaoTest extends BaseTest {
 
 		System.out.println(shop.toString());
 	}
+
+	@Test
+	public void testSelectShopListAndCount() {
+
+		Area area = new Area();
+		ShopCategory shopCategory = new ShopCategory();
+		PersonInfo personInfo = new PersonInfo();
+		List<Shop> shopList = null;
+
+		/**
+		 * 可输入的查询条件： 1.商铺名（要求模糊查询） 2.区域Id 3.商铺状态 4.商铺类别 5.owner
+		 * 
+		 * 
+		 * 首先按照单个条件进行单元测试，然后组合测试
+		 **/
+		
+		// 1.商铺名（要求模糊查询）
+		Shop shopCondition = new Shop();
+		shopCondition.setShopName("咖啡");
+		
+		// 1.1 数据库中只有3条数据符合 ，我们分页条件 取出5条，即全部取出 验证rowIndex 和 pageSize
+		shopList = shopDao.selectShopList(shopCondition, 0, 5);
+		Assert.assertEquals(3, shopList.size());
+		
+		int count1 = shopDao.selectShopCount(shopCondition);
+		Assert.assertEquals(3, count1);
+
+		// 1.2 数据库中只有3条数据符合 ，我们分页条件 取出2条，即取出前两条 验证rowIndex 和 pageSize
+		shopList = shopDao.selectShopList(shopCondition, 0, 2);
+		Assert.assertEquals(2, shopList.size());
+		
+		// 总数依然是3条
+		int count2 = shopDao.selectShopCount(shopCondition);
+		Assert.assertEquals(3, count2);
+
+		// 为了不影响测试， 新实例化出来一个Shop
+
+		// 2.区域Id 库表中符合条件的记录有10条 areaId=1 10条 areaId=2 3条
+		Shop shopCondition2 = new Shop();
+		area.setAreaId(1);
+		shopCondition2.setArea(area);
+		shopList = shopDao.selectShopList(shopCondition2, 0, 99);
+		Assert.assertEquals(10, shopList.size());
+		
+		area.setAreaId(2);
+		shopCondition2.setArea(area);
+		shopList = shopDao.selectShopList(shopCondition2, 0, 99);
+		Assert.assertEquals(3, shopList.size());
+		
+		
+		// 3.商铺状态 EnableStatus=0 12条 EnableStatus=1 1条
+		Shop shopCondition3 = new Shop();
+		shopCondition3.setEnableStatus(0);
+		shopList = shopDao.selectShopList(shopCondition3, 0, 99);
+		Assert.assertEquals(12, shopList.size());
+
+		shopCondition3.setEnableStatus(1);
+		shopList = shopDao.selectShopList(shopCondition3, 0, 99);
+		Assert.assertEquals(1, shopList.size());
+				
+				
+		// 4.商铺类别
+		// shop_category_id = 1 9条数据
+		// shop_category_id = 2 3条数据
+		// shop_category_id = 3 1条数据
+		Shop shopCondition4 = new Shop();
+
+		shopCategory.setShopCategoryId(1L);
+		shopCondition4.setShopCategory(shopCategory);
+		shopList = shopDao.selectShopList(shopCondition4, 0, 99);
+		Assert.assertEquals(9, shopList.size());
+
+		shopCategory.setShopCategoryId(2L);
+		shopCondition4.setShopCategory(shopCategory);
+		shopList = shopDao.selectShopList(shopCondition4, 0, 99);
+		Assert.assertEquals(3, shopList.size());
+
+		shopCategory.setShopCategoryId(3L);
+		shopCondition4.setShopCategory(shopCategory);
+		shopList = shopDao.selectShopList(shopCondition4, 0, 99);
+		Assert.assertEquals(1, shopList.size());
+		
+		// 5.owner_id=1 13条 其余0条
+		Shop shopCondition5 = new Shop();
+		personInfo.setUserId(1L);
+		shopCondition5.setOwner(personInfo);
+		shopList = shopDao.selectShopList(shopCondition5, 0, 99);
+		Assert.assertEquals(13, shopList.size());
+
+		personInfo.setUserId(877L);
+		shopCondition5.setOwner(personInfo);
+		shopList = shopDao.selectShopList(shopCondition5, 0, 99);
+		Assert.assertEquals(0, shopList.size());
+
+		// 组合场景不全面，仅列几个
+
+		// 组合场景 owner_id =1 shop_name like %咖啡%
+		Shop shopCondition6 = new Shop();
+		personInfo.setUserId(1L);
+		shopCondition6.setOwner(personInfo);
+		shopCondition6.setShopName("咖啡");
+		shopList = shopDao.selectShopList(shopCondition6, 0, 99);
+		Assert.assertEquals(3, shopList.size());
+
+		int count6 = shopDao.selectShopCount(shopCondition6);
+		Assert.assertEquals(3, count6);
+
+		// 组合场景 area_id =1 shop_name like %咖啡% owner_id=1
+		Shop shopCondition7 = new Shop();
+		personInfo.setUserId(1L);
+		area.setAreaId(1);
+		shopCondition7.setOwner(personInfo);
+		shopCondition7.setArea(area);
+		shopCondition7.setShopName("咖啡");
+		shopList = shopDao.selectShopList(shopCondition7, 0, 99);
+		Assert.assertEquals(2, shopList.size());
+
+		int count7 = shopDao.selectShopCount(shopCondition7);
+		Assert.assertEquals(2, count7);
+	}
+
+
+
 }
